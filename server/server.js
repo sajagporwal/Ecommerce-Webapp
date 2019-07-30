085b0ae1634c2b3021497ec253b0bd7a3524d4a5
Sajag Porwal <sajagporwal@gmail.com>
2019-07-30 11:17:25 +0530

    made /* as * in server for that  NODE_ENV

--- server/server.js
+++ server/server.js
@@ -351,8 +351,8 @@ app.post('/api/users/login',(req,res)=>{
 if(process.env.NODE_ENV === 'production')
 {
     const path = require('path');
-    app.get('/*',(req,res)=>{
-        res.sendFile(path,resolve(__dirname,'../client','build','index.html'));
+    app.get('*',(req,res)=>{
+        res.sendFile(path,resolve(__dirname,'../client','build','index.html'))
     })
 }
 
