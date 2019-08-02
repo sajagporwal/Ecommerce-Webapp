89c3d31d4c41e9fd9502c7093a44f98ea96fce8b
Sajag Porwal <sajagporwal@gmail.com>
2019-08-02 06:43:32 +0530

    rev back

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,9 +1,9 @@
 const proxy = require('http-proxy-middleware')
  
-// module.exports = function(app) {
-//   app.use(proxy('/api/', { target: 'http://localhost:3002/' }))
-// }
-
 module.exports = function(app) {
-  app.use(proxy('/api/', { target: 'https://aqueous-coast-92529.herokuapp.com/' }))
+  app.use(proxy('/api/', { target: 'http://localhost:3002/' }))
 }
+
+// module.exports = function(app) {
+//   app.use(proxy('/api/', { target: 'https://aqueous-coast-92529.herokuapp.com/' }))
+// }
