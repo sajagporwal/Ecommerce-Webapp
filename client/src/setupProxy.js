70db5efe7b4bca886e79a8136d7a879f80304b19
Sajag Porwal <sajagporwal@gmail.com>
2019-08-02 06:32:16 +0530

    setupProxy changes

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,5 +1,9 @@
 const proxy = require('http-proxy-middleware')
  
+// module.exports = function(app) {
+//   app.use(proxy('/api/', { target: 'http://localhost:3002/' }))
+// }
+
 module.exports = function(app) {
-  app.use(proxy('/api/', { target: 'http://localhost:3002/' }))
+  app.use(proxy('/api/', { target: 'https://aqueous-coast-92529.herokuapp.com/' }))
 }
